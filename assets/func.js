3f0381e2e8f910c88686b04b723777dfacf7857d
iamswet <isitswet@gmail.com>
2024-07-13 23:29:49 +0530

    a

--- assets/func.js
+++ assets/func.js
@@ -2,6 +2,6 @@ function toggleMenu(){
     const menu=document.querySelector(".menu-links");
     const icon=document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
-    menu.classList.toggle("open")
+    icon.classList.toggle("open")
 
 }
