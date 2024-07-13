894143bcd2233316486bee74fa567d8b5f51dcdb
iamswet <isitswet@gmail.com>
2024-07-13 23:12:26 +0530

    a

---
+++ assets/func.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    menu.classList.toggle("open")
+
+}
